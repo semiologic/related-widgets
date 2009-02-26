7983578d3d9c08753328a886f53ad357df0daf08
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-02-26 11:13:56 +0000

    drop package url
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/related-widgets/trunk@859 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- related-widgets.php
+++ related-widgets.php
@@ -3,10 +3,9 @@
 Plugin Name: Related Widgets
 Plugin URI: http://www.semiologic.com/software/widgets/related-widgets/
 Description: WordPress widgets that let you list related posts or pages. Requires that you tag your posts and pages.
-Version: 2.2.2
+Version: 2.2.3 alpha
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
-Update Package: https://members.semiologic.com/media/plugins/related-widgets/related-widgets.zip
 */
 
 /*
