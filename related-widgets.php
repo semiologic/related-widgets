dc66dac6e4f0f2c23e45ad4e7efc7af4cac274e7
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-06-10 19:10:51 +0000

    show titles in widgets admin
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/related-widgets/trunk@1729 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- related-widgets.php
+++ related-widgets.php
@@ -638,7 +638,9 @@ class related_widget extends WP_Widget {
 		echo '<p>'
 			. '<label>'
 			. __('Title:', 'related-widgets') . '<br />' . "\n"
-			. '<input type="text" size="20" name="' . $this->get_field_name('title') . '" class="widefat"'
+			. '<input type="text" size="20" class="widefat"'
+				. ' id="' . $this->get_field_id('title') . '"'
+				. ' name="' . $this->get_field_name('title') . '"'
 				. ' value="' . esc_attr($title) . '"'
 				. ' />'
 			. '</label>'
